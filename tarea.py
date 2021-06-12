695f61b3145c144ed26815c7d318a48a0658823c
britonohe0418 <anonymous>
2021-06-12 00:48:19 -0500

    ej1 metodo usoIf en la clase Condicion.

--- tarea.py
+++ tarea.py
@@ -6,3 +6,11 @@ class Condicion:
         self.numero2 = num2
         # numero = num1 + num2
         self.numero3 = num1
+    
+    def usoIf(self):
+        if self.numero1 == self.numero2:
+            print("El numero1: {} es igual al numero2: {}.".format(self.numero1, self.numero2))
+        elif self.numero1 == self.numero3:
+            print("El numero1: {} es igual al numero3: {}.".format(self.numero1, self.numero3))
+        else:
+            print("Estos números no son iguales.")
