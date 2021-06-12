a3b02f8a6a41d4bf872cb8a611d162c15eec2751
britonohe0418 <anonymous>
2021-06-12 00:41:14 -0500

    ej1 metodo constructor de la clase Condicion.

---
+++ tarea.py
@@ -0,0 +1,8 @@
+class Condicion:
+    contador = 0
+
+    def __init__(self, num1=0, num2=0):
+        self.numero1 = num1
+        self.numero2 = num2
+        # numero = num1 + num2
+        self.numero3 = num1
